0987aca5148bcfd5563d92197118a882903416d0
slapa02 <slapa02@gmail.com>
2022-03-26 19:34:35 +0100

    fixed tests bugs

--- main_test.py
+++ main_test.py
@@ -1,12 +1,10 @@
 from main import draw_six_numbers
 
+
 def test_draw_numbers():
 
     for _ in range(500):
-        # given
-        # when
-        drow_numbers = draw_six_numbers()
-        #then
-        assert len(drow_numbers) == 6
-        assert all([x >= 1 for x in drow_numbers])
-        assert all([x <= 49 for x in drow_numbers])
+        draw_numbers = draw_six_numbers(list(range(1,50)))
+        assert len(draw_numbers) == 6
+        assert all([x >= 1 for x in draw_numbers])
+        assert all([x <= 49 for x in draw_numbers])
